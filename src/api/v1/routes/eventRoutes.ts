83018b387c6d5446a5302802e9749f8e67321813
Seth <sboyer2@academic.rrc.ca>
2026-02-13 09:54:13 -0600

    Defined Router and API Routers

---
+++ src/api/v1/routes/eventRoutes.ts
@@ -0,0 +1,9 @@
+import express, { Router } from "express";
+
+export const eventRouter: Router = express.Router()
+
+eventRouter.get("/events/:id")
+eventRouter.put("/events/:id")
+eventRouter.delete("/events/:id")
+eventRouter.get("/events")
+eventRouter.post("/events")
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Express } from "express";
+import { eventRouter } from "./api/v1/routes/eventRoutes";
+import morgan from "morgan";
 
 // Initialize Express application
 
 const app: Express = express();
 
-// Define a route
+
+app.use(morgan("combined"));
+app.use(express.json());
+app.use("/api/v1/", eventRouter)
+
 
 app.get("/", (req, res) => {
 
