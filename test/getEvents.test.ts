107b2dcf2cc0128f2692394eec518e0abf5b2bf2
Seth <sboyer2@academic.rrc.ca>
2026-02-21 22:15:26 -0600

    Added getEvents test and figured out how to test w/o firebase

---
+++ test/getEvents.test.ts
@@ -0,0 +1,21 @@
+import { events } from "../src/api/v1/services/eventServices"
+import * as services from "../src/api/v1/services/eventServices"
+
+describe("Get Events test", () => {
+    beforeEach(() => {
+        events.length = 0
+        events.push (
+            { id: 1, name: "Evil George Bush meeting", date: new Date(), capacity: 50, registrationCount: 0, status: "Active", category: "Tech" },
+            { id: 2, name: "George Washington funtime", date: new Date(), capacity: 50, registrationCount: 0, status: "Active", category: "Tech" }
+        )
+    })
+  it("Should return all events", async () => {
+
+    // Act
+    const result = await services.getAllEventsService
+
+    // Assert
+    expect(result.length).toBe(2)
+
+  })
+})
