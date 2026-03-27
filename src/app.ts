d656eacf45c33cbfc389509543f8ca2e28e77e42
Seth <sboyer2@academic.rrc.ca>
2026-03-27 10:13:24 -0500

    Added cors config

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app: Express = express();
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(getHelmetConfig());
-app.use(cors());
+app.use(cors(getCorsOptions()));
 app.use("/api/v1/", eventRouter);
 
 app.get("/", (req, res) => {
